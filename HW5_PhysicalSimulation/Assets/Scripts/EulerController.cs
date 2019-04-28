9bd547db60c46b145576dc46eba36df7345b073f
Breezeeee <S-J-T-U-j-f-x@sjtu.edu.cn>
2019-04-29 00:05:10 +0800

    part1(not finished)

---
+++ HW5_PhysicalSimulation/Assets/Scripts/EulerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EulerController : MonoBehaviour
+{
+    public float g = 9.8f;
+    private float l;
+    private float theta;
+    private float omega;
+
+    private Transform Top;
+    private Transform Sphere;
+
+    void Start()
+    {
+        Top = GameObject.FindGameObjectWithTag("EulerTop").GetComponent<Transform>();
+        Sphere = this.GetComponent<Transform>();
+
+        l = Vector3.Distance(Top.position, Sphere.position);
+        theta = Vector3.Angle(Sphere.position - Top.position, new Vector3(0, -1, 0));
+        omega = 0.0f;
+    }
+
+    void Update()
+    {
+        Sphere.RotateAround(Top.position, new Vector3(0, 0, -1), omega);
+        theta = Vector3.Angle(Sphere.position - Top.position, new Vector3(0, -1, 0));
+        float deltaOmega = g / l * Mathf.Sin(Mathf.Deg2Rad * theta) * Time.deltaTime;
+        if (Top.position.x > Sphere.position.x)
+        {
+            omega = omega - deltaOmega;
+        }
+        else
+        {
+            omega = omega + deltaOmega;
+        }
+    }
+}
