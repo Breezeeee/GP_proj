92bcb32229393cc3e4ceac3e0dc7ae1e8a7ed841
Breezeeee <S-J-T-U-j-f-x@sjtu.edu.cn>
2019-04-29 01:35:14 +0800

    save

---
+++ HW5_PhysicalSimulation/Assets/Scripts/Hair/HairGenerate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairGenerate : MonoBehaviour
+{
+    public GameObject prefab;
+    public List<GameObject> hairSet;
+    public int hairSize = 200;
+
+    void Start()
+    {
+        hairSet = new List<GameObject>();
+        for (int idx = 0; idx < hairSize; idx++)
+        {
+            GameObject go = Instantiate(prefab) as GameObject;
+            go.transform.SetParent(transform);
+            go.transform.RotateAround(new Vector3(0, go.transform.position.y, 0), new Vector3(0, 1, 0), 240 / hairSize * (idx + 1));
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ HW5_PhysicalSimulation/Assets/Scripts/Pendulum/TrapzoidController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapzoidController : MonoBehaviour
+{
+    public float g = 9.8f;
+    public float mass = 0.01f;
+    private float l;
+    private float theta;
+    private float omega;
+
+    private Transform Top;
+    private Transform Sphere;
+
+    void Start()
+    {
+        Top = GameObject.FindGameObjectWithTag("TrapzoidTop").GetComponent<Transform>();
+        Sphere = this.GetComponent<Transform>();
+
+        l = Vector3.Distance(Top.position, Sphere.position);
+        theta = Vector3.Angle(Sphere.position - Top.position, new Vector3(0, -1, 0));
+        omega = 0.0f;
+    }
+
+    void Update()
+    {
+        Sphere.RotateAround(Top.position, new Vector3(0, 0, -1), omega);
+        theta = Vector3.Angle(Sphere.position - Top.position, new Vector3(0, -1, 0));
+        float deltaOmega = g / l * Mathf.Sin(Mathf.Deg2Rad * theta) * mass;
+        float tmpOmega;
+        if (Top.position.x > Sphere.position.x)
+        {
+            tmpOmega = omega - deltaOmega;
+        }
+        else
+        {
+            tmpOmega = omega + deltaOmega;
+        }
+        omega = (tmpOmega + omega) / 2;
+    }
+}
